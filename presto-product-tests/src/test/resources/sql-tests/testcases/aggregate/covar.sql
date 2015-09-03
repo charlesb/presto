c5aeee49f1f56dc15b46adc1477169c19c4c8608
Anu Sudarsan <anu.sudarsan@teradata.com>
2015-09-03 17:32:15 -0400

    Fix diff in sql-tests results

--- presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/covar.sql
+++ presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/covar.sql
@@ -1,2 +1,2 @@
 -- database: presto; groups: aggregate; tables: orders
-select round(covar_pop(o_totalprice, o_orderkey), 4) , round(covar_samp(o_totalprice, o_orderkey),4) from orders
+select round(cast(covar_pop(o_totalprice, o_orderkey) as numeric), 1) , round(cast(covar_samp(o_totalprice, o_orderkey) as numeric),1) from orders
