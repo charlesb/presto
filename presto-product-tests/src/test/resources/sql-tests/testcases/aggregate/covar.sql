9cd6736e87baf0722fc3f16381bd9ebe35419ba3
Anu Sudarsan <anu.sudarsan@teradata.com>
2015-09-03 17:34:37 -0400

    Fix sql-tests
    Remove cast added by mistake

--- presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/covar.sql
+++ presto-product-tests/src/test/resources/sql-tests/testcases/aggregate/covar.sql
@@ -1,2 +1,2 @@
 -- database: presto; groups: aggregate; tables: orders
-select round(cast(covar_pop(o_totalprice, o_orderkey) as numeric), 1) , round(cast(covar_samp(o_totalprice, o_orderkey) as numeric),1) from orders
+select round(covar_pop(o_totalprice, o_orderkey), 1) , round(covar_samp(o_totalprice, o_orderkey),1) from orders
